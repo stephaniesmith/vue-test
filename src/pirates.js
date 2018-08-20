1d1a822aced6f87c08f7b94ba079164bc996b36f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-20 11:32:31 -0700

    basic setup

---
+++ src/pirates.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'Pirate Joe',
+    role: 'PIRATE'
+  },
+  {
+    name: 'Pirate Jane',
+    role: 'PIRATE'
+  },
+  {
+    name: 'Pirate Jose',
+    role: 'PIRATE'
+  }
+];
